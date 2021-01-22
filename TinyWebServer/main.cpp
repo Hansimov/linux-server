2d6006dcff5b5635684d9ede95e42395a4d5a943
Hansimov <591172499@qq.com>
2021-01-22 16:53:11 +0800

    + Update TinyWebServer config

--- TinyWebServer/main.cpp
+++ TinyWebServer/main.cpp
@@ -4,8 +4,8 @@ int main(int argc, char *argv[])
 {
     //需要修改的数据库信息,登录名,密码,库名
     string user = "root";
-    string passwd = "root";
-    string databasename = "qgydb";
+    string passwd = "q";
+    string databasename = "yourdb";
 
     //命令行解析
     Config config;
